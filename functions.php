f3a689d86af856e085def5d7268420ed10726e3f
Eric Denton <eric@iamericanartist.com>
2017-02-18 15:01:51 -0600

    first commit

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+
+add_action('wp_enqueue_scripts', 'tsc_wp_enqueue_scripts');
+
+function tsc_wp_enqueue_scripts()
+{
+    wp_enqueue_style('twentyseventeen-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('twentyseventeen-child-style', get_stylesheet_directory_uri() . '/style.css');
+}
+
+add_action('after_setup_theme', 'tsc_after_setup_theme');
+
+function tsc_after_setup_theme()
+{
+    register_nav_menu('evergreen-content', __('Evergreen Menu', 'twentyseventeen-child'));
+}
+
+add_action('widgets_init', 'tsc_widget_init');
+
+function tsc_widget_init() {
+    register_sidebar(
+        array(
+            'id' => 'footer-widget-area',
+            'name' => __('Footer Widget Area', 'twentyseventeen-child'),
+            'description' => __('Adds a widget', 'twentyseventeen-child'),
+        )
+    );
+}
---
+++ welcome-to-our-blog.php
@@ -0,0 +1,59 @@
+<?php // Template Name: Welcome to Our Blog
+
+get_header(); ?>
+
+    <div class="wrap">
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main" role="main">
+
+                <h1><?php the_title(); ?></h1>
+
+                <?php if ( have_posts() ): ?>
+                    <?php while ( have_posts() ): ?>
+                        <?php the_post(); ?>
+                        <?php the_content(); ?>
+                    <?php endwhile; ?>
+                <?php endif; ?>
+
+                <hr/>
+
+                <p>Check out these awesome posts:</p>
+
+                <?php
+                wp_nav_menu( array(
+                    'theme_location' => 'evergreen-content',
+                    'menu_class'     => 'evergreen-content',
+                ) );
+                ?>
+
+                <hr/>
+
+                <?php
+                $query = new WP_Query( array(
+                    'post_type'      => 'post',
+                    'posts_per_page' => 5,
+                ) );
+                ?>
+                <?php if ( $query->have_posts() ): ?>
+                    <p>Check out our latest posts:</p>
+                    <ul>
+                        <?php while ( $query->have_posts() ): ?>
+                            <?php $query->the_post(); ?>
+                            <li>
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php the_title(); ?>
+                                </a>
+                            </li>
+                        <?php endwhile; ?>
+                        <?php wp_reset_postdata(); ?>
+                    </ul>
+                <?php endif; ?>
+
+            </main><!-- #main -->
+        </div><!-- #primary -->
+    </div><!-- .wrap -->
+
+<div id="sidebar-footer"></div>
+
+
+<?php get_footer(); ?>
